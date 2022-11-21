c685b481fe91de465246d13ff57c9d2cbd143ed4
Regina Rguez <regina.rguez.c@gmail.com>
2022-11-21 20:44:00 +0100

    MVC done

--- src/controllers/UsuarioController.php
+++ src/controllers/UsuarioController.php
@@ -2,23 +2,23 @@
 require_once 'models/usuario.php';
 
 class usuarioController{
-	
+
 	public function index(){
 		echo "Controlador Usuarios, Acción index";
 	}
-	
+
 	public function registro(){
 		require_once 'views/usuario/registro.php';
 	}
-	
+
 	public function save(){
 		if(isset($_POST)){
-			
+
 			$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
 			$apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
 			$email = isset($_POST['email']) ? $_POST['email'] : false;
 			$password = isset($_POST['password']) ? $_POST['password'] : false;
-			
+
 			if($nombre && $apellidos && $email && $password){
 				$usuario = new Usuario();
 				$usuario->setNombre($nombre);
@@ -40,7 +40,7 @@ class usuarioController{
 		}
 		header("Location:".base_url.'usuario/registro');
 	}
-	
+
 	public function login(){
 		if(isset($_POST)){
 			// Identificar al usuario
@@ -48,34 +48,34 @@ class usuarioController{
 			$usuario = new Usuario();
 			$usuario->setEmail($_POST['email']);
 			$usuario->setPassword($_POST['password']);
-			
+
 			$identity = $usuario->login();
-			
+
 			if($identity && is_object($identity)){
 				$_SESSION['identity'] = $identity;
-				
+
 				if($identity->rol == 'admin'){
 					$_SESSION['admin'] = true;
 				}
-				
+
 			}else{
 				$_SESSION['error_login'] = 'Identificación fallida !!';
 			}
-		
+
 		}
 		header("Location:".base_url);
 	}
-	
+
 	public function logout(){
 		if(isset($_SESSION['identity'])){
 			unset($_SESSION['identity']);
 		}
-		
+
 		if(isset($_SESSION['admin'])){
 			unset($_SESSION['admin']);
 		}
-		
+
 		header("Location:".base_url);
 	}
-	
+
 } // fin clase
